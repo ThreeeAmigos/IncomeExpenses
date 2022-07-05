f33e57b435ba0ac9d74ce57726f1d5e78c3b9660
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-07-05 16:22:16 +0100

    Add initial test for Expense

---
+++ services/src/test/java/com/threeAmigos/services/models/ExpenseTest.java
@@ -0,0 +1,90 @@
+package com.threeAmigos.services.models;
+
+import static org.junit.Assert.*;
+
+public class ExpenseTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.Test
+    public void setId() {
+    }
+
+    @org.junit.Test
+    public void getDate() {
+    }
+
+    @org.junit.Test
+    public void setDate() {
+    }
+
+    @org.junit.Test
+    public void getName() {
+    }
+
+    @org.junit.Test
+    public void setName() {
+    }
+
+    @org.junit.Test
+    public void getPlace() {
+    }
+
+    @org.junit.Test
+    public void setPlace() {
+    }
+
+    @org.junit.Test
+    public void getAmount() {
+    }
+
+    @org.junit.Test
+    public void setAmount() {
+    }
+
+    @org.junit.Test
+    public void getCategory() {
+    }
+
+    @org.junit.Test
+    public void setCategory() {
+    }
+
+    @org.junit.Test
+    public void getNecessityIndex() {
+    }
+
+    @org.junit.Test
+    public void setNecessityIndex() {
+    }
+
+    @org.junit.Test
+    public void getPerson() {
+    }
+
+    @org.junit.Test
+    public void setPerson() {
+    }
+
+    @org.junit.Test
+    public void getPurpose() {
+    }
+
+    @org.junit.Test
+    public void setPurpose() {
+    }
+
+    @org.junit.Test
+    public void isDirectDebit() {
+    }
+
+    @org.junit.Test
+    public void setDirectDebit() {
+    }
+
+    @org.junit.Test
+    public void getId() {
+    }
+}
