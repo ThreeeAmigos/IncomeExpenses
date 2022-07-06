ce56e8540147acf927ecc2b5654ac521d2c066b4
Hansel Kang <43307207+hanselkang@users.noreply.github.com>
2022-07-06 19:54:13 +0100

    Deleted unnecessary imports in repositories

--- services/src/main/java/com/threeAmigos/services/repositories/ExpenseRepository.java
+++ services/src/main/java/com/threeAmigos/services/repositories/ExpenseRepository.java
@@ -1,6 +1,5 @@
 package com.threeAmigos.services.repositories;
 
-import com.threeAmigos.services.models.Category;
 import com.threeAmigos.services.models.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
