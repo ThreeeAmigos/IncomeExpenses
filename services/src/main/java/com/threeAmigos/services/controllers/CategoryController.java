2c1cd6ba6bee67588cd78c685868787c1739f56a
Cameron Johnston <cjohnston4765@gmail.com>
2022-07-07 16:10:11 +0100

    Controllers and endpoints added; some repository methods will have to be added to fetch all endpoints.

---
+++ services/src/main/java/com/threeAmigos/services/controllers/CategoryController.java
@@ -0,0 +1,41 @@
+package com.threeAmigos.services.controllers;
+
+import com.threeAmigos.services.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CategoryController {
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+
+    /**
+     * Handles routes and filters:
+     *  GET  /categories
+     *  GET  /categories?categoryid=1
+     * @return `ResponseEntity<List<Category>>`
+     */
+    @GetMapping(value = "/categories")
+    public ResponseEntity getAllCategoriesAndFilters(
+            @RequestParam(required = false, name = "category") Long categoryId
+
+    ) {
+        // GET  /households?householdid=1
+        if (categoryId != null) {
+            return new ResponseEntity(categoryRepository.findByCategory(categoryId), HttpStatus.OK);
+        }
+
+        // GET  /categories
+        return new ResponseEntity(categoryRepository.findAll(), HttpStatus.OK);
+
+    }
+
+}
