7ce84a2c1975937a67aed828ccf7a39667cf12ab
Hansel Kang <43307207+hanselkang@users.noreply.github.com>
2022-07-09 18:44:22 +0100

    RegiterPage Structures working, current balance inputs are not satisfying

--- client/src/containers/MainContainer.js
+++ client/src/containers/MainContainer.js
@@ -1,10 +1,15 @@
 import React from "react";
 import FetchCategory from "../components/FetchCategory";
 import FetchExpenses from "../components/FetchExpenses";
+import RegisterPage from "../components/RegisterPage";
 
 const MainContainer = () => {
   return (
-    <FetchCategory></FetchCategory>
+    <>
+      <RegisterPage />
+      <FetchCategory />
+
+    </>
   )
 }
 
