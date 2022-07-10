54bd9f86fea76c61b7440b977439e7e0bb554323
Hansel Kang <43307207+hanselkang@users.noreply.github.com>
2022-07-10 11:06:03 +0100

    Added HouseholdController/ Method PUT by ID

--- services/src/main/java/com/threeAmigos/services/controllers/HouseholdController.java
+++ services/src/main/java/com/threeAmigos/services/controllers/HouseholdController.java
@@ -1,15 +1,16 @@
 package com.threeAmigos.services.controllers;
 
 import com.threeAmigos.services.models.Household;
+import com.threeAmigos.services.models.Purpose;
 import com.threeAmigos.services.repositories.HouseholdRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import sun.security.krb5.internal.Ticket;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class HouseholdController {
@@ -39,4 +40,18 @@ public class HouseholdController {
 
     }
 
+    @PutMapping("/purposes/{id}")
+    public ResponseEntity<Household> putHousehold(@RequestBody Household newHousehold, @PathVariable Long id) {
+        householdRepository.findById(id)
+                .map(household -> {
+                    household.setTarget(newHousehold.getTarget());
+                    return householdRepository.save(household);
+                })
+                .orElseGet(() -> {
+                    newHousehold.setId(id);
+                    return householdRepository.save(newHousehold);
+                });
+        return new ResponseEntity<>(newHousehold, HttpStatus.ACCEPTED);
+
+    }
 }
