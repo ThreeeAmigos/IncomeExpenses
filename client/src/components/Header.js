2b7c805831f9259eabf8a9957b5f8ae24bb18a50
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-07-12 14:10:57 +0100

    Initial header implementation

---
+++ client/src/components/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import RegisterPurpose from "./RegisterPurpose";
+import RegisterIncome from "./RegisterIncome";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import styled from "styled-components";
+
+const MainHeader = styled.header`
+  display: flex;
+  flex-direction: row;
+  height: 10vh;
+  border-bottom: 0.5vh solid black;
+  justify-content: space-around;
+  align-items: center;
+  margin-bottom: 2vh;
+`;
+
+const HeaderItem = styled.div`
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: 200;
+  width: fit-content;
+`
+
+const Header = () => {
+  return (
+    <MainHeader>
+      <HeaderItem>Element one</HeaderItem>
+      <HeaderItem>Element two</HeaderItem>
+    </MainHeader>
+  )
+}
+
+export default Header;
--- client/src/containers/MainContainer.js
+++ client/src/containers/MainContainer.js
@@ -2,6 +2,7 @@ import React from "react";
 import FetchCategory from "../components/FetchCategory";
 import FetchExpenses from "../components/FetchExpenses";
 import Register from "../components/Register";
+import Header from "../components/Header";
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,6 +14,7 @@ import {
 const MainContainer = () => {
   return (
     <>
+      <Header></Header>
       <Register />
       <FetchCategory />
 
