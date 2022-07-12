b298286e2d4955497d4dda308d99649889d8ef61
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-07-12 14:47:51 +0100

    Add initial implementation of Footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
-import MainContainer from './containers/MainContainer';
+import "./App.css";
+import MainContainer from "./containers/MainContainer";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+  justify-content: space-between;
+`;
 
 function App() {
   return (
-    <div className="App">
+    <Wrapper>
       <MainContainer></MainContainer>
-    </div>
+    </Wrapper>
   );
 }
 
--- client/src/containers/MainContainer.js
+++ client/src/containers/MainContainer.js
@@ -3,23 +3,18 @@ import FetchCategory from "../components/FetchCategory";
 import FetchExpenses from "../components/FetchExpenses";
 import Register from "../components/Register";
 import Header from "../components/Header";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Footer from "../components/Footer";
 
 const MainContainer = () => {
   return (
     <>
-      <Header></Header>
+      <Header />
       <Register />
       <FetchCategory />
-
+      <Footer />
     </>
-  )
-}
+  );
+};
 
 export default MainContainer;
