39b53b6e379b68bf71ed006931ba21251333a169
Hansel Kang <43307207+hanselkang@users.noreply.github.com>
2022-07-13 15:27:46 +0100

    expense list sorted

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,60 +4,19 @@ import RegisterIncome from "./RegisterIncome";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import styled from "styled-components";
 
-const MainHeader = styled.header`
-  display: flex;
-  flex-direction: row;
-  height: 10vh;
-  border-bottom: 0.5vh solid black;
-  justify-content: space-around;
-  align-items: center;
-  margin-bottom: 2vh;
-`;
-
-
 
 const Header = () => {
   return (
-    <MainHeader>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/dashboard">MINT</a>
+        <a class="navbar-brand" href="/expense" color="white">Expense</a>
+        <a class="navbar-brand" href="/income" color="white">Income</a>
+        <a class="navbar-brand" href="/setup" color="white">Setup</a>
+        <a class="navbar-brand" href="/registerPurpose" color="white">Register</a>
 
-          <div class="collapse navbar-collapse" id="navbarColor01">
-            <ul class="navbar-nav me-auto">
-              <li class="nav-item">
-                <a class="nav-link active" href="#">Home
-                  <span class="visually-hidden">(current)</span>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Features</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">About</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                <div class="dropdown-menu">
-                  <a class="dropdown-item" href="#">Action</a>
-                  <a class="dropdown-item" href="#">Another action</a>
-                  <a class="dropdown-item" href="#">Something else here</a>
-                  <div class="dropdown-divider"></div>
-                  <a class="dropdown-item" href="#">Separated link</a>
-                </div>
-              </li>
-            </ul>
-            
-          </div>
-        </div>
-      </nav>
-    </MainHeader>
+      </div>
+    </nav>
   );
 };
 
